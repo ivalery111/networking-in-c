0918e93669e541483f88016aa55635d481f05315
Valery Ivanov <ivalery111@gmail.com>
2020-12-24 17:31:55 +0300

    src: adds bind function

--- src/internal/network_internal.h
+++ src/internal/network_internal.h
@@ -1,7 +1,9 @@
 #ifndef NETWORK_NETWORK_INTERNAL_H
 #define NETWORK_NETWORK_INTERNAL_H
 
+#include <netinet/in.h>
 #include <stdint.h>
+#include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
@@ -9,6 +11,7 @@
 #include "network.h"
 
 int _net_open_udp_socket();
+int _net_bind_socket(const int sock, const uint32_t addr, const uint16_t port);
 
 int _net_close_socket(const int sock);
 
