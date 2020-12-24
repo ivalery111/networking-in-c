2764c557a55f5f62b192aba8ba3d80e95d1e08d5
Valery Ivanov <ivalery111@gmail.com>
2020-12-24 18:53:30 +0300

    src: adds read udp buffer function

--- src/internal/network_internal.h
+++ src/internal/network_internal.h
@@ -10,8 +10,12 @@
 
 #include "network.h"
 
+#define NET_MAX_BUFFER_SIZE 1024
+
 int _net_open_udp_socket();
 int _net_bind_socket(const int sock, const uint32_t addr, const uint16_t port);
+
+ssize_t _net_read_buffer_udp(IN const int sender, IN char *buffer);
 
 int _net_close_socket(const int sock);
 
