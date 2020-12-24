72cc6988e32ac4f6936de364fb621c3b265ffa6b
Valery Ivanov <ivalery111@gmail.com>
2020-12-24 19:07:41 +0300

    src: adds send udp buffer function

--- src/internal/network_internal.h
+++ src/internal/network_internal.h
@@ -16,6 +16,9 @@ int _net_open_udp_socket();
 int _net_bind_socket(const int sock, const uint32_t addr, const uint16_t port);
 
 ssize_t _net_read_buffer_udp(IN const int sender, IN char *buffer);
+ssize_t _net_send_buffer_udp(IN int          receiver,
+                             IN const char * buffer,
+                             IN const size_t buffer_size);
 
 int _net_close_socket(const int sock);
 
