9507ee2503a01838e2464457c407c2c8c87b5119
Valery Ivanov <ivalery111@gmail.com>
2020-12-24 16:51:44 +0300

    src: adds open udp socket func and close socket func

--- src/internal/network_internal.h
+++ src/internal/network_internal.h
@@ -2,7 +2,14 @@
 #define NETWORK_NETWORK_INTERNAL_H
 
 #include <stdint.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 #include "network.h"
+
+int _net_open_udp_socket();
+
+int _net_close_socket(const int sock);
 
 #endif
---
+++ tests/test_open_socket.c
@@ -0,0 +1,11 @@
+#include <assert.h>
+
+#include "../src/internal/network_internal.h"
+
+int main(void) {
+  int sock = _net_open_udp_socket();
+  assert(sock != (-1));
+
+  int result = _net_close_socket(sock);
+  assert(result == 0);
+}
